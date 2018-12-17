a70de68d147e8f87e6273a379f3db5e7e58f257d
yaziid <cantinanv@hotmail.com>
2018-12-17 19:24:09 +0100

    Ajout du calcul du rapport euro/cl d'ethanol ainsi que des modifications mineur du design (Icon BottomNavigationBar et couleur)

--- app/src/main/java/com/example/yazid/myapplication/Calcul.java
+++ app/src/main/java/com/example/yazid/myapplication/Calcul.java
@@ -56,18 +56,36 @@ public class Calcul extends AppCompatActivity {
         final EditText nom_biere = (EditText)findViewById(R.id.nom_biere);
         final EditText prix_biere = (EditText)findViewById(R.id.prix_biere);
         final EditText alcool_biere = (EditText)findViewById(R.id.alcool_biere);
+        final EditText quantite_biere = (EditText)findViewById(R.id.quantite_biere);
 
         bouton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!Strings.isEmptyOrWhitespace(nom_biere.getText().toString()) &&
                         !Strings.isEmptyOrWhitespace(prix_biere.getText().toString()) &&
-                        !Strings.isEmptyOrWhitespace(alcool_biere.getText().toString())){
+                        !Strings.isEmptyOrWhitespace(alcool_biere.getText().toString()) &&
+                        !Strings.isEmptyOrWhitespace(quantite_biere.getText().toString())){
                     nom_bieres.add(nom_biere.getText().toString());
-                    prix_bieres.add(nom_biere.getText().toString());
+
+                    float rapport = CalculerRapportEuroCentilitre(Float.parseFloat(prix_biere.
+                                    getText().toString()),
+                            Float.parseFloat(alcool_biere.getText().toString()),
+                            Float.parseFloat(quantite_biere.getText().toString()));
+
+                    prix_bieres.add(Float.toString(rapport) + " euros/cl");
+
                     adapter.notifyDataSetChanged();
                 }
             }
         });
+    }
+
+    private float CalculerRapportEuroCentilitre(float prix_biere, float alcool_biere,
+                                                float quantite_biere){
+        float quantite_ethanol = (alcool_biere * quantite_biere) / 100.0f;
+        float rapport = prix_biere / quantite_ethanol;
+
+        return rapport;
+
     }
 }
